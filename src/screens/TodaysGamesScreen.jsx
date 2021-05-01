1d5bda734ef9e2cae1f24b263bf4ef66c9cfb215
jcash102 <joannam.cash@gmail.com>
2021-05-01 19:50:53 -0400

    add react navigation and bottom nav

---
+++ src/screens/TodaysGamesScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TodaysGamesScreen = () => {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>List of Today's games</Text>
+		</View>
+	);
+};
+
+export { TodaysGamesScreen };
